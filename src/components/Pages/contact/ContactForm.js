81db6d169b8b4db778bf82a026e064b114bd4be7
chukkydave <chukkydave@gmail.com>
2022-08-08 11:20:05 +0100

    remove comments and add react icons on footer page

--- src/components/Pages/contact/ContactForm.js
+++ src/components/Pages/contact/ContactForm.js
@@ -2,12 +2,8 @@ import { Continer } from '../../StyledComponents/StyleSetTwo/Continer.styled';
 import imgGridWhite from '../../../Assets/grid-white.svg';
 import contact from '../../../Assets/contact-us.svg';
 export const ContactForm = () => {
-	// const pElem = {
-	//     lineHeight:
-	// }
 	return (
 		<Continer>
-			{/* <div> */}
 			<div className="container mt-5">
 				<div className="d-flex pt-5">
 					<img src={imgGridWhite} alt="white grid" />
@@ -28,7 +24,6 @@ export const ContactForm = () => {
 
 					<div className="col-md-6 col-lg-6 col-sm-12 mt-lg-5 mt-md-5 mt-xl-5 formDiv">
 						<div className="card">
-							{/* <div className="container"> */}
 							<div className="form-group">
 								<label>First Name *</label>
 								<input type="text" className="form-control" />
@@ -47,10 +42,8 @@ export const ContactForm = () => {
 								<textarea className="form-control" rows="3" />
 							</div>
 							<button className="btn btn-block btn-primary">Send Message</button>
-							{/* </div> */}
 						</div>
 					</div>
-					{/* </div> */}
 				</div>
 			</div>
 		</Continer>
